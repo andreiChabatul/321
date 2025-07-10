fcd556137ec8d36dff2b12d2b627ff0fad3213a3
andreiChabatul <100202306+andreiChabatul@users.noreply.github.com>
2025-07-10 21:25:27 +0300

    feat search result

---
+++ mooz/src/components/ResultSearch.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex items-center justify-start gap-[1.7rem]">
+    <p class="text-[2.2rem]">{{ `You searched for: ${temp}` }}</p>
+    <button
+      class="bg-Color_blue pointer-events-none h-[3rem] px-[1.5rem] text-[1.4rem] text-white rounded-[0.5rem] font-medium"
+    >
+      {{ `${13123} results` }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const temp = ref("vdvd");
+</script>
--- mooz/src/views/MainView.vue
+++ mooz/src/views/MainView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="pt-[5rem] px-[19rem]">
-    <HeaderComponent />
+    <HeaderComponent class="mb-[4.5rem]" />
+    <ResultSearch />
   </div>
 </template>
 
 <script setup lang="ts">
 import HeaderComponent from "@/components/HeaderComponent.vue";
+import ResultSearch from "@/components/ResultSearch.vue";
 </script>
