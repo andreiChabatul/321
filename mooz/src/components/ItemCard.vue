1a7858de0140c1f6b13901d20f959d36cfec62b6
andreiChabatul <andreichabatul@gmail.com>
2025-07-11 10:00:02 +0300

    item card done

--- mooz/src/components/ItemCard.vue
+++ mooz/src/components/ItemCard.vue
@@ -1,22 +1,35 @@
 <template>
   <div class="w-[24.5rem] h-[38rem] text-[1.6rem] text-left">
     <div
-      class="w-full h-[27rem] border-solid border-[1px] border-Color_blue rounded-[1rem] mb-[2.3rem] flex items-center justify-center"
+      class="w-full h-[27rem] overflow-hidden border-solid border-[1px] rounded-[1rem] mb-[2.3rem] flex items-center justify-center"
+      :class="{ 'border-Color_blue': errorImg }"
     >
-      <img src="../assets/placeholder.png" alt="placeholder" />
+      <img
+        class="w-full"
+        v-if="!errorImg"
+        :src="item?.Poster"
+        alt="poster"
+        @error="handleImageError"
+      />
+      <img v-else src="../assets/placeholder.png" alt="placeholder" />
     </div>
-    <p>Name: Batman</p>
-    <p>Name: Batman</p>
-    <p>Name: Batman</p>
-    <p>Name: Batman</p>
+    <p>Name: {{ item?.Title }}</p>
+    <p>Year: {{ item?.Year }}</p>
+    <p>imdbID: {{ item?.imdbID }}</p>
+    <p>Type: {{ item?.Type }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Movie } from "@/stores/types";
-import { defineProps } from "vue";
+import { defineProps, ref } from "vue";
 
 defineProps<{
   item: Movie;
 }>();
+const errorImg = ref(false);
+
+const handleImageError = () => {
+  errorImg.value = true;
+};
 </script>
